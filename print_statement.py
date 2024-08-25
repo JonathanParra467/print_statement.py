b468ece9db41c6e73451914a076ebb14d833b84d
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-08-25 16:12:40 -0500

    Add files via upload

---
+++ print_statement.py
@@ -0,0 +1,9 @@
+#Knock Knock Joke Program
+print("Knock knock!")
+print("Whos there?")
+print("Frank.")
+print("Frank \nwho?")
+print("\"Frank you\" for asking, \nits me.")
+
+#As one line using escape characters
+print("Knock knock. \nWhos there? \nFrank. \nFrank who? \n\"Frank you\" for asking, its me.")
